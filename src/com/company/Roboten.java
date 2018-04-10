9605edf25bb92f574f526ab2cc101fea1b7c8020
zeitge <hermannythe@gmail.com>
2018-04-11 01:22:45 +0200

    La til litt forskjellige overrides som minimumskravet krever.

--- src/com/company/Roboten.java
+++ src/com/company/Roboten.java
@@ -1,7 +1,42 @@
 package com.company;
 
-import robocode.Robot;
+import robocode.*;
 
 public class Roboten extends Robot {
+
+    @Override
+    public void run() {
+        while(true) {
+            ahead(100);
+            ahead(-100);
+            turnGunLeft(90);
+            fire(3);
+        }
+    }
+
+    @Override
+    public void onBulletHit(BulletHitEvent e) {
+
+    }
+
+    @Override
+    public void onHitByBullet(HitByBulletEvent e) {
+
+    }
+
+    @Override
+    public void onHitRobot(HitRobotEvent e) {
+
+    }
+
+    @Override
+    public void onHitWall(HitWallEvent e) {
+
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent e) {
+
+    }
 
 }
