48aefe32bae3225b437858be63246863efa42006
brave-face <raggsokkjappen@hotmail.com>
2018-04-14 13:07:41 +0200

    This isn't even my final form

--- src/com/company/Roboten.java
+++ src/com/company/Roboten.java
@@ -2,15 +2,23 @@ package com.company;
 
 import robocode.*;
 
+import java.awt.*;
+
 public class Roboten extends AdvancedRobot {
+
+
 
     @Override
     public void run() {
+        setBodyColor(Color.red);
+        setGunColor(Color.black);
+
         while(true) {
-            ahead(100);
-            ahead(-100);
-            turnGunLeft(90);
-            fire(3);
+            setTurnLeft(10000);
+            setMaxVelocity(4);
+            setMaxTurnRate(10);
+            ahead(10000);
+
         }
     }
 
@@ -26,17 +34,37 @@ public class Roboten extends AdvancedRobot {
 
     @Override
     public void onHitRobot(HitRobotEvent e) {
+// (bare til testing)
+        if (e.getBearing() > -10 && e.getBearing() < 10) {
+            fire(3);
+        }
+        if (e.isMyFault()) {
+            turnLeft(10);
+        }
 
     }
 
     @Override
     public void onHitWall(HitWallEvent e) {
-
+// (bare til testing)
+        turnLeft(10);
     }
 
     @Override
     public void onScannedRobot(ScannedRobotEvent e) {
-
+        if (getVelocity() < 6){
+            setMaxVelocity(6);
+        }
+        else{
+            setMaxVelocity(4);
+        }
+        fire(3);
+    }
+    public void onWin (WinEvent e) {
+        System.out.println("GG, losers!");
+    }
+    public void onDeath (DeathEvent e) {
+        System.out.println("Hacker!!@");
     }
 
 }
