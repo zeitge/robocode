14de9b6293b1c7908e24026c8f11734f89d25795
zeitge <38223583+zeitge@users.noreply.github.com>
2018-04-17 11:47:22 +0200

    botten vår - nå med farger og kommentarer

--- src/com/company/Frankenstein.java
+++ src/com/company/Frankenstein.java
@@ -13,22 +13,27 @@ public class Frankenstein extends AdvancedRobot {
         while(true) {
             fram = fram + 100;
             setTurnRight(10000);
-            setMaxVelocity(fart++);
+            setMaxVelocity(fart);
             ahead(fram);
         }
+
     }
 
     public void onHitByBullet(HitByBulletEvent e) {
-        setAhead(-(Math.random()*1000+1000));
+        setAhead(-(Math.random() * 1000 + 1000));
+        // Når roboten blir truffet av en bullet, vil roboten
+        // kjøre tilbake.
     }
 
     public void onHitWall(HitWallEvent e) {
         setBack(100);
         turnLeft(10);
+        //Når den treffer veggen, går den tilbake 100 og snur 10 grader til venstre.
     }
 
     public void onBulletHit(BulletHitEvent e) {
         setTurnRight(1);
+        // Når bulleten treffer en annen robot, juster 1 grader til høyre.
     }
 
     public void onHitRobot(HitRobotEvent e) {
@@ -40,25 +45,33 @@ public class Frankenstein extends AdvancedRobot {
             setTurnRight(100);
             setAhead(100);
         }
+        //Når roboten vår kolliderer med en annen robot, blir den illsint og rød.
+        //Dersom roboten kolliderer i front, så skal den kjøre tilbake og svinge.
+        //Dersom roboten kolliderer bak, så skal den svinge til høyre og kjøre fram,
+        //for å komme unna.
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
         if(getVelocity() < 6) {
-            setMaxVelocity(6);
+            fart = 6;
         } else {
-            setMaxVelocity(4);
+            fart = 4;
         }
+        //Veksler mellom fart på 6 og 4.
 
         turnGunRight(Utils.normalRelativeAngleDegrees((getHeading() + e.getBearing()) - getGunHeading()));
         setTurnRight(e.getBearing()+90);
         setFire(3);
+        //Tracker roboten den scanner, og prøver å streife rundt den i en høyresving, samtidig som den skyter.
     }
 
     public void onWin (WinEvent e) {
         System.out.println("GG, losers!");
+        //Skriver ut i konsollen dersom man vinner.
     }
 
     public void onDeath (DeathEvent e) {
         System.out.println("Hacker!!@");
+        //Anklager andre for juks dersom man taper.
     }
 }
