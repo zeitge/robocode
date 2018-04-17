fb659404b9cc68d1297c3ca3578765b4a79130f5
zeitge <38223583+zeitge@users.noreply.github.com>
2018-04-17 07:04:20 +0200

    blablabla

--- src/com/company/Roboten.java
+++ src/com/company/Roboten.java
@@ -1,42 +1,59 @@
 package com.company;
 
 import robocode.*;
+import robocode.util.Utils;
 
-public class Roboten extends Robot {
+public class Roboten extends AdvancedRobot {
+    int timesHit = 0;
+    int firePower = 3;
+    boolean right = true;
+    int direction = 100;
+    String hitRobotName;
 
-    @Override
-    public void run() {
-        while(true) {
-            ahead(100);
-            ahead(-100);
-            turnGunLeft(90);
-            fire(3);
-        }
+
+        public void run() {
+                setAdjustRadarForRobotTurn(true);
+                while(true){
+                    turnRadarRight(360);
+                }
     }
 
-    @Override
     public void onBulletHit(BulletHitEvent e) {
+        timesHit++;
 
     }
 
-    @Override
     public void onHitByBullet(HitByBulletEvent e) {
-
+        changePath();
+        setAhead(-(Math.random()*1000+1000));
     }
 
-    @Override
     public void onHitRobot(HitRobotEvent e) {
-
+        changePath();
+        setAhead(-(Math.random()*1000+1000));
     }
 
-    @Override
+    private void changePath() {
+            setAhead(direction);
+            direction = -direction;
+    }
+
     public void onHitWall(HitWallEvent e) {
-
+        changePath();
     }
 
-    @Override
     public void onScannedRobot(ScannedRobotEvent e) {
-
+            turnGunRight(Utils.normalRelativeAngleDegrees((getHeading() + e.getBearing()) - getGunHeading()));
+        setTurnRight(e.getBearing()+90);
+        changePath();
+        if(e.getDistance()>30) {
+            firePower = 1;
+        } else if(e.getDistance()<30 && e.getDistance()>10) {
+            firePower = 2;
+        } else if(e.getDistance()<10) {
+            firePower = 3;
+        }
+        setFire(firePower);
     }
 
 }
